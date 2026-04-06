8e6cb5e3d0d25fc7ac0bf4fd39fa0aa4d9023179
Luísa Michelin Riqueti <luisa.michelinr@gmail.com>
2026-04-06 13:40:28 -0300

    Mudança no CORS

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins="*")
+CORS(app,
+     origins=[
+         "http://localhost:5173",
+         "http://127.0.0.1:5173"
+     ],
+     supports_credentials=True
+)
 app.config.from_pyfile('config.py')
 
 host = app.config['DB_HOST']
